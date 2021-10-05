34372f3a7882a0116e6e4e08ffa96e3fa26283eb
kavitaparsewar <parsewarkavita@gmail.com>
2021-10-05 23:56:06 +0530

    [kavita] added node.cs file of Linked List using Stack

---
+++ Node.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day14_Stack_Queue_Assignment.Stack
+{
+    class Node
+    {
+        public int data;
+        public Node next;
+        public Node(int data)
+        {
+            this.data = data;
+        }
+    }
+}
