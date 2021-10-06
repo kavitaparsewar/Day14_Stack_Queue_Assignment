d188ab2f5bb3b5ec80bf69a6c50c017d9a2adbf0
kavitaparsewar <parsewarkavita@gmail.com>
2021-10-06 22:24:15 +0530

    [kavita] added node file

---
+++ Linked_List_Using_Queue/Node.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day14_Stack_Queue_Assignment.Linked_List_Using_Queue
+{
+    class Node
+    {
+        public int data;
+        public Node next;
+        public Node(int data)
+        {
+            this.data = data;
+        }
+    }
+}
